314d1745c76a263172193a7e40275643626670e4
gyuuuutan <gyuuuutan.pei@gmail.com>
2017-01-17 16:57:56 +0900

    Save new_chat_group
    リレーションにaccepts_nested_attributes_forを追加し、chat_groupsテーブルとgroup_usersテーブル
    にフォームで入力された値が一括保存されるようにした。

--- app/controllers/chat_groups_controller.rb
+++ app/controllers/chat_groups_controller.rb
@@ -1,7 +1,20 @@
 class ChatGroupsController < ApplicationController
 
+  def index
+  end
+
   def new
     @chat_group = ChatGroup.new
+  end
+
+  def create
+    ChatGroup.create(chat_group_params)
+    redirect_to controller: :chat_groups, action: :index
+  end
+
+  private
+  def chat_group_params
+    params.require(:chat_group).permit(:group_name, { :user_ids => []})
   end
 
 end
