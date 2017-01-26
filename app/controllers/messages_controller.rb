e57d12fb46bd4d5c322506f362e20db69f3a476a
gyuuuutan <gyuuuutan.pei@gmail.com>
2017-01-26 14:30:26 +0900

    Add create action
    Createアクションの実装。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,21 @@
 class MessagesController < ApplicationController
   def index
     @chat_group = ChatGroup.find(params[:chat_group_id])
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to action: :index
+    else
+      flash[:alert] = "メッセージが送信できませんでした。"
+      redirect_to action: :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, chat_group_id: params[:chat_group_id])
   end
 end
