ab35c58827335d08220962256412167af7ea00b5
jumpyoshim <jumpyoshim@gmail.com>
2017-01-26 21:24:22 +0900

    Change redirect_to to render

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,8 @@
 class MessagesController < ApplicationController
+
+  before_action :set_chat_group, only: %i(index create)
+
   def index
-    @chat_group = ChatGroup.find(params[:chat_group_id])
     @message = Message.new
   end
 
@@ -10,12 +12,16 @@ class MessagesController < ApplicationController
       redirect_to action: :index
     else
       flash[:alert] = "メッセージを入力してください。"
-      redirect_to action: :index
+      render action: :index
     end
   end
 
   private
   def message_params
     params.require(:message).permit(:body, :image).merge(user_id: current_user.id, chat_group_id: params[:chat_group_id])
+  end
+
+  def set_chat_group
+    @chat_group = ChatGroup.find(params[:chat_group_id])
   end
 end
