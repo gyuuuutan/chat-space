b4e0c82f320b7efbb31a2d7eb399946c8171adc0
jumpyoshim <jumpyoshim@gmail.com>
2017-01-27 11:43:54 +0900

    Use association for removing unnecessary code
    Paramsにcurrent_user.idをわざわざ渡さなくても済むようにアソシエーションを用いた。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.new(message_params)
     if @message.save
       redirect_to chat_group_messages_path
     else
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, chat_group_id: params[:chat_group_id])
+    params.require(:message).permit(:body, :image).merge(chat_group_id: params[:chat_group_id])
   end
 
   def set_chat_group
