a9e39eea8523d94bbd8a305e65cd25b6c20c3643
jumpyoshim <jumpyoshim@gmail.com>
2017-01-30 17:38:09 +0900

    Add devise test_helper
    messages_controller_spec.rbでdeviseのエルパーメソッドを使えるように設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,10 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  # FactoryGirlの記述を省略するための追記
   config.include FactoryGirl::Syntax::Methods
+  # deviseのテストヘルパーをロード
+  config.include Devise::TestHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
