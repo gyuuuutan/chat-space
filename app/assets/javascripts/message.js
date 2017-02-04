8b4496cac5d40666327822320abdc0e0d88bf720
jumpyoshim <jumpyoshim@gmail.com>
2017-02-04 13:24:57 +0900

    Correction of variable setting

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function() {
   // メッセージ送信情報を追加していく関数
   function buildHTML(message) {
     var user_name = $('<p class="chat-message__name">').append(message.name);
-    var message_time = $('<p class="chat-message__time">').append(message.created_at);
+    var message_time = $('<p class="chat-message__time">').append(message.time);
     var message_body = $('<p class="chat-message__body">').append(message.body);
     var html = $('<li class="chat-message">').append(user_name, message_time, message_body);
     return html;
