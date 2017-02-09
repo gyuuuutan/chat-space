afb61a192206582236fa17af0eb16441a636a8da
gyuuuutan <gyuuuutan.pei@gmail.com>
2017-02-09 18:07:53 +0900

    Change buildHTML
    メッセージ送信情報を追加していく関数を書き換え

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,26 @@
 $(function() {
   // メッセージ送信情報を追加していく関数
   function buildHTML(message) {
-    var user_name = $('<p class="chat-message__name">').append(message.name);
-    var message_time = $('<p class="chat-message__time">').append(message.time);
-    var message_body = $('<p class="chat-message__body">').append(message.body);
-    var html = $('<li class="chat-message">').append(user_name, message_time, message_body);
+    if(message.image != 0) {
+      var insertMessageHTML =
+      '<br>' +
+      '<img class="chat-message__image">' +
+          message_image +
+      '</imag>' +
+    }
+    var html =
+    '<li class="chat-message">' +
+      '<p class="chat-message__name">' +
+          message.name +
+      '</p>' +
+      '<p class="chat-message__time">' +
+          message.time +
+      '</p>' +
+      '<p class="chat-message__body">' +
+          message.body +
+      '</p>' +
+      insertMessageHTML +
+    '</li>'
     return html;
   }
 
